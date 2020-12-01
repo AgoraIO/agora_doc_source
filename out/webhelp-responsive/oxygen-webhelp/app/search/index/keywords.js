eb72ac97be40da5266899331d0f28563a83bf140
wangjie-agora <wangjie@agora.io>
2020-12-01 18:42:41 +0800

    convert dita

---
+++ out/webhelp-responsive/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,15 @@
+define(function() {var keywords=[{w:"agora",p:["p0"]},{w:"java",p:["p0"]},{w:"api",p:["p0"]},{w:"reference",p:["p0"]},{w:"for",p:["p0"]},{w:"android",p:["p0"]},{w:"setchannelprofile",p:["p1"]},{w:"joinchannel",p:["p2"]},{w:"setclientrole",p:["p3"]},{w:"rtcengine",p:["p4"]},{w:"onuserjoined",p:["p5"]},{w:"irtcengineeventhandler",p:["p6"]},{w:"\u6570\u636E",p:["p7"]},{w:"\u636E\u7C7B",p:["p7"]},{w:"\u7C7B\u578B",p:["p7"]}];
+var ph={};
+ph["p0"]=[0, 1, 2, 3, 4, 5];
+ph["p1"]=[6];
+ph["p2"]=[7];
+ph["p3"]=[8];
+ph["p4"]=[9];
+ph["p5"]=[10];
+ph["p6"]=[11];
+ph["p7"]=[12, 13, 14];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
---
+++ out/webhelp-responsive/oxygen-webhelp/app/search/index/stopwords.js
@@ -0,0 +1,4 @@
+define(function() {
+// Auto generated list of analyzer stop words that must be ignored by search.
+return ["but","be","with","such","if","for","no","will","not","are","and","their","then","this","on","into","a","or","there","in","that","they","was","is","it","at","the","as","s","t","these","by","to","of","www"];
+});
