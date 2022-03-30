390bbe0cd3c003026485fa95f8dc01fd671b055a
Lutkin Wang <yamasite@qq.com>
2022-03-30 15:51:57 +0800

    Update check_unity_ng_prototype.py

--- xml2json/check_unity_ng_prototype.py
+++ xml2json/check_unity_ng_prototype.py
@@ -123,7 +123,17 @@ def main():
             code4 = code3.replace(" ", "")
             code5 = code4.replace("\n", "")
 
-            if content5.find(code5) == -1:
+            if code5 == "":
+                write_log("No. " + str(i) + " Mismatch found")
+                write_log("\n")
+                write_log("-------------------------------------------------------------------------------")
+                write_log("-------------------------------------------------------------------------------")
+                write_log("For the DITA file: " + file)
+                write_log("This prototype is empty!!!")
+                write_log("-------------------------------------------------------------------------------")
+                write_log("-------------------------------------------------------------------------------")
+                write_log("\n")
+            elif content5.find(code5) == -1:
                 write_log("No. " + str(i) + " Mismatch found")
                 i = i + 1
                 write_log("\n")
@@ -131,16 +141,6 @@ def main():
                 write_log("-------------------------------------------------------------------------------")
                 write_log("For the DITA file: " + file)
                 write_log("This prototype in DITA cannot be located in the source code: \n " + code + "\n")
-                write_log("-------------------------------------------------------------------------------")
-                write_log("-------------------------------------------------------------------------------")
-                write_log("\n")
-            elif code5 == "":
-                write_log("No. " + str(i) + " Mismatch found")
-                write_log("\n")
-                write_log("-------------------------------------------------------------------------------")
-                write_log("-------------------------------------------------------------------------------")
-                write_log("For the DITA file: " + file)
-                write_log("This prototype is empty!!!")
                 write_log("-------------------------------------------------------------------------------")
                 write_log("-------------------------------------------------------------------------------")
                 write_log("\n")
