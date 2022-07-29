5fd837d6c85439731d6eaea3ffe0a881f60c3df6
yamasite <null>
2022-07-29 10:56:52 +0000

    🔄 Synced local 'xml2json/' with remote 'xml2json/'

--- xml2json/xml2json.py
+++ xml2json/xml2json.py
@@ -168,6 +168,7 @@ elif defined_path_text == "cs":
 # cpp_full_path
 # Other types of full path
 rust_topicref_list = []
+json_hide_id_list = []
 dita_file_tree = ET.parse(defined_path)
 dita_file_root = dita_file_tree.getroot()
 for topicref in dita_file_root.iter("keydef"):
@@ -181,6 +182,23 @@ print("--------------- Topic ref list ------------------------")
 print(rust_topicref_list)
 print("--------------- Topic ref list ------------------------")
 
+# Collect the hide API which mark props="hide" or "cn" in <topichead> or <keydef>
+for topichead in dita_file_root.iter("topichead"):
+    is_hide_topichead: bool = True if topichead.get("props") is not None and topichead.get("props") == "hide" or topichead.get("props") is not None and topichead.get("props") == "cn" else False
+
+    for keydef in topichead.iter("keydef"):
+        if keydef.get("href") is not None:
+            dita_id = path.basename(keydef.get("href")).replace(".dita", "")
+            if is_hide_topichead:
+                json_hide_id_list.append(dita_id)
+            elif keydef.get("props") is not None and keydef.get("props") == "hide":
+                json_hide_id_list.append(dita_id)
+
+
+print("--------------- Hide id list ------------------------")
+print(json_hide_id_list)
+print("--------------- Hide id list ------------------------")
+
 # Target platform
 
 # List of platforms
@@ -188,6 +206,16 @@ props_platform_list = ["windows", "rust", "java", "python", "csharp", "objective
 
 
 def create_json_from_xml(working_dir, file_dir, android_path, cpp_path, rust_path, platform="rust"):
+
+    text = ""
+
+    with open(file_dir, "r", encoding='utf-8') as f:
+        text= f.read()
+        text = re.sub('\s+(?=<)', '', text)
+
+    with open(file_dir, "w", encoding='utf-8') as f:
+        f.write(text)
+
     tree = ET.parse(file_dir)
     root = tree.getroot()
 
@@ -223,7 +251,7 @@ def create_json_from_xml(working_dir, file_dir, android_path, cpp_path, rust_pat
             if platform_tag not in child.get("props") and "native" not in child.get(
                     "props") and child.get("props") != "rtc" and child.get("props") != "rtc-ng" or remove_sdk_type in child.get("props") or platform_tag not in child.get(
                      "props") and "native" in child.get(
-                 "props") and platform_tag != "windows" and platform_tag != "macos" and platform_tag != "android" and platform_tag != "ios" and child.get("props") != "rtc" and child.get("props") != "rtc-ng":
+                 "props") and platform_tag != "cpp" and platform_tag != "macos" and platform_tag != "android" and platform_tag != "ios" and child.get("props") != "rtc" and child.get("props") != "rtc-ng" or child.get("props") == "hide" or child.get("props") == "cn":
                  print("------------------- Tag to remove ---------------------------")
                  print(child)
                  print(child.text)
@@ -710,7 +738,7 @@ def create_json_from_xml(working_dir, file_dir, android_path, cpp_path, rust_pat
                     for text in keydef.itertext():
                         href_text = href_text + text
                     xref.text = href_text
-                href_text = ""    
+                href_text = ""
 
             if sys.platform == 'darwin' or sys.platform == 'linux':
                 print("macOS")
@@ -732,8 +760,8 @@ def create_json_from_xml(working_dir, file_dir, android_path, cpp_path, rust_pat
                     dir = None
                     print(xref.text)
                 else:
-                    dir = None 
-            """         
+                    dir = None
+            """
             if dir is not None:
                 print(dir)
                 dita_file_tree = ET.parse(dir)
@@ -848,7 +876,7 @@ def create_json_from_xml(working_dir, file_dir, android_path, cpp_path, rust_pat
             if platform_tag not in child.get("props") and "native" not in child.get(
                     "props") and child.get("props") != "rtc" and child.get("props") != "rtc-ng" or remove_sdk_type in child.get("props") or platform_tag not in child.get(
                      "props") and "native" in child.get(
-                 "props") and platform_tag != "windows" and platform_tag != "macos" and platform_tag != "android" and platform_tag != "ios" and child.get("props") != "rtc" and child.get("props") != "rtc-ng":
+                 "props") and platform_tag != "cpp" and platform_tag != "macos" and platform_tag != "android" and platform_tag != "ios" and child.get("props") != "rtc" and child.get("props") != "rtc-ng":
                  print("------------------- Tag to remove ---------------------------")
                  print(child)
                  print(child.text)
@@ -870,7 +898,7 @@ def create_json_from_xml(working_dir, file_dir, android_path, cpp_path, rust_pat
             if platform_tag not in child.get("props") and "native" not in child.get(
                     "props") and child.get("props") != "rtc" and child.get("props") != "rtc-ng" or remove_sdk_type in child.get("props") or platform_tag not in child.get(
                      "props") and "native" in child.get(
-                 "props") and platform_tag != "windows" and platform_tag != "macos" and platform_tag != "android" and platform_tag != "ios" and child.get("props") != "rtc" and child.get("props") != "rtc-ng":
+                 "props") and platform_tag != "cpp" and platform_tag != "macos" and platform_tag != "android" and platform_tag != "ios" and child.get("props") != "rtc" and child.get("props") != "rtc-ng":
                  print("------------------- Tag to remove ---------------------------")
                  print(child)
                  print(child.text)
@@ -893,7 +921,7 @@ def create_json_from_xml(working_dir, file_dir, android_path, cpp_path, rust_pat
             if platform_tag not in child.get("props") and "native" not in child.get(
                     "props") and child.get("props") != "rtc" and child.get("props") != "rtc-ng" or remove_sdk_type in child.get("props") or platform_tag not in child.get(
                      "props") and "native" in child.get(
-                 "props") and platform_tag != "windows" and platform_tag != "macos" and platform_tag != "android" and platform_tag != "ios" and child.get("props") != "rtc" and child.get("props") != "rtc-ng":
+                 "props") and platform_tag != "cpp" and platform_tag != "macos" and platform_tag != "android" and platform_tag != "ios" and child.get("props") != "rtc" and child.get("props") != "rtc-ng":
                  print("------------------- Tag to remove ---------------------------")
                  print(child)
                  print(child.text)
@@ -995,12 +1023,16 @@ def create_json_from_xml(working_dir, file_dir, android_path, cpp_path, rust_pat
                 param_name = child.find("pt").text
                 if param_name is None and child.find("./pt/ph") is not None:
                     param_name = child.find("./pt/ph").text
-                    
-                else:
+
+                elif child.text is not None:
                     print("Something unexpected happened for " + child.text)
-                  
+
+                elif child.text is None:
+                    print("No text for this node")
+                    print(child)
+
                 if child.find("./pd") is not None:
-                
+
                     for text in child.find("./pd").itertext():
                         if text is not None:
                             print(text)
@@ -1063,6 +1095,7 @@ def create_json_from_xml(working_dir, file_dir, android_path, cpp_path, rust_pat
     data['description'] = api_desc
     data['parameters'] = json_array
     data['returns'] = return_values.strip("\n ")
+    data['is_hide'] = True if api_id in json_hide_id_list else False
 
     print(data)
 
@@ -1113,13 +1146,12 @@ def merge_JsonFiles(files):
     warning = json.loads("{\"id\": \"enum_warningcode\", \"name\": \"WarningCode\", \"description\": \"Warning codes. See https://docs.agora.io/en/Interactive%20Broadcast/error_rtc.\", \"parameters\": [], \"returns\": \"\"}")
 
     result.append(error)
-    result.append(warning)    
+    result.append(warning)
 
     with open(json_file, 'w', encoding="utf-8") as output_file:
         json.dump(result, output_file, ensure_ascii=False, indent=4)
 
 
-    
 
 
 def replace_newline():
@@ -1186,13 +1218,13 @@ def replace_newline():
     replaced_file_text = re.sub('id="callback_iaudioframeobserverbase_', 'id="callback_iaudioframeobserver_', replaced_file_text)
     replaced_file_text = re.sub('"LOCAL_VIDEO_STREAM_ERROR_DEVICE_NOT_FOUND": ""', '"LOCAL_VIDEO_STREAM_ERROR_DEVICE_NOT_FOUND": "8: Fails to find a local video capture device."', replaced_file_text)
     replaced_file_text = re.sub('{ "LOCAL_VIDEO_STREAM_ERROR_DEVICE_BUSY": "3: The local video capturing device is in use." },', '{ "LOCAL_VIDEO_STREAM_ERROR_DEVICE_BUSY": "3: The local video capturing device is in use." }, { "LOCAL_VIDEO_STREAM_ERROR_CAPTURE_FAILURE": "4: The local video capture fails. Check whether the capturing device is working properly." },', replaced_file_text)
-    
-    
+
+
     # ------------------ Special processing for Flutter classes ------------------------------------------
 
     # { "LOCAL_VIDEO_STREAM_ERROR_DEVICE_BUSY": "3: The local video capturing device is in use." },
     # { "LOCAL_VIDEO_STREAM_ERROR_DEVICE_BUSY": "3: The local video capturing device is in use." }, { "LOCAL_VIDEO_STREAM_ERROR_CAPTURE_FAILURE": "4: The local video capture fails. Check whether the capturing device is working properly." },
-    
+
     input_file.close()
 
     output_file = open(json_file, 'w', encoding="utf-8")
