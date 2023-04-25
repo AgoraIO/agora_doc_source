abb877f851a89cc4db0a72c94c84262ba7b413f7
lemon-chen <null>
2023-04-25 08:10:16 +0000

    🔄 synced local 'xml2json/' with remote 'xml2json/'

---
+++ xml2json/code_to_check/csharp_ng/IAudioPcmFrameSink.cs
@@ -0,0 +1,27 @@
+namespace Agora.Rtc
+{
+    ///
+    /// <summary>
+    /// The audio frame observer for the media player.
+    /// </summary>
+    ///
+    public abstract class IAudioPcmFrameSink
+    {
+        ///
+        /// <summary>
+        /// Occurs each time the player receives an audio frame.
+        /// After registering the audio frame observer, the callback occurs every time the player receives an audio frame, reporting the detailed information of the audio frame.
+        /// </summary>
+        ///
+        /// <param name="videoFrame"> Audio frame information. See AudioPcmFrame .</param>
+        ///
+        /// <returns>
+        /// Reserved for future use.
+        /// </returns>
+        ///
+        public virtual bool OnFrame(AudioPcmFrame frame)
+        {
+            return true;
+        }
+    }
+}
