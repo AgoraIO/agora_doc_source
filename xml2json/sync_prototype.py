e95e476b5cbb776bdd90a3782d9c55a2a151b2f1
jinyu <jinyu@agora.io>
2023-11-02 10:22:26 +0800

    + sync keysmap

--- xml2json/sync_prototype.py
+++ xml2json/sync_prototype.py
@@ -1,40 +1,55 @@
 import os
 import re
 import argparse
+import shutil
 
-parser = argparse.ArgumentParser(description="Prototype syncer")
+def sync_prototype(src_dir, dest_dir):
+    src_api_dir = os.path.join(src_dir, 'API')
+    dest_api_dir = os.path.join(dest_dir, 'API')
 
-parser.add_argument("--src_dir",help="src dir",action="store")
-parser.add_argument("--dest_dir", help="dest dir", action="store")
+    for file_name in os.listdir(src_api_dir):
+        if file_name.startswith(('api_', 'callback_', 'class_')):
+            src_file = os.path.join(src_api_dir, file_name)
+            dest_file = os.path.join(dest_api_dir, file_name)
 
-args = vars(parser.parse_args())
+            with open(src_file, 'r') as file:
+                content = file.read()
+                pattern = r'<section id="prototype".*?</section>'
+                proto = re.findall(pattern, content, flags=re.DOTALL)
+                if not proto:
+                    print(f"No proto in cn file: {src_file}")
+                    continue
 
-src_dir = args['src_dir']
-dest_dir = args['dest_dir']
+            if os.path.exists(dest_file):
+                with open(dest_file, 'r+') as file:
+                    content = file.read()
+                    updated_content = re.sub(r'<section id="prototype".*?</section>', proto[0], content, flags=re.DOTALL)
+                    file.seek(0)
+                    file.write(updated_content)
+                    file.truncate()
+            else:
+                print(f"No corresponding en file: {dest_file}")
 
-prefixes = ('api_', 'callback_', 'class_')
+def sync_keysmap(src_dir, dest_dir):
+    src_config_dir = os.path.join(src_dir, 'config')
+    dest_config_dir = os.path.join(dest_dir, 'config')
 
-for file_name in os.listdir(src_dir):
+    for file_name in os.listdir(src_config_dir):
+        if file_name.startswith('keys-rtc-ng-api'):
+            src_file = os.path.join(src_config_dir, file_name)
+            dest_file = os.path.join(dest_config_dir, file_name)
+            shutil.copy2(src_file, dest_file)
 
-    if file_name.startswith(prefixes):
+def main():
+    parser = argparse.ArgumentParser(description="Prototype & Keysmap syncer")
+    parser.add_argument("--src_dir", help="source directory", action="store")
+    parser.add_argument("--dest_dir", help="destination directory", action="store")
+    args = vars(parser.parse_args())
+    src_dir = args['src_dir']
+    dest_dir = args['dest_dir']
 
-        cn_path = os.path.join(src_dir, file_name)
-        with open(cn_path, 'r') as cn_file:
-            cn_file_content = cn_file.read()
-            pattern = r'<section id="prototype".*?</section>'
-            proto = re.findall(pattern, cn_file_content, flags=re.DOTALL)
-            if not proto:
-                print(f"No proto in cn file: {cn_path}")
-                continue
+    sync_prototype(src_dir, dest_dir)
+    sync_keysmap(src_dir, dest_dir)
 
-        en_path = os.path.join(dest_dir, file_name)
-        try:
-            with open(en_path, 'r+') as en_file:
-                en_file_content = en_file.read()
-                en_file.seek(0)
-                updated_content = re.sub(r'<section id="prototype".*?</section>', proto[0], en_file_content, flags=re.DOTALL)
-                en_file.write(updated_content)
-                en_file.truncate()
-        except FileNotFoundError:
-            print(f"No corresponding en file: {en_path}")
-            continue
+if __name__ == '__main__':
+    main()
